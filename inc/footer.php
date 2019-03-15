bab2f22f4e8b845b2e1b6d4dc18f81385b5124f1
mannye3 <aboajahemmanuel@gmail.com>
2019-03-15 21:48:48 +0100

    update

---
+++ inc/footer.php
@@ -0,0 +1,92 @@
+
+<div id="footer" class="sticky-footer">
+	<!-- Main -->
+	<div class="container">
+		<div class="row">
+			<div class="col-md-5 col-sm-6">
+				<img class="footer-logo" src="images/logo.png" alt="">
+				<br><br>
+				<p>Morbi convallis bibendum urna ut viverra. Maecenas quis consequat libero, a feugiat eros. Nunc ut lacinia tortor morbi ultricies laoreet ullamcorper phasellus semper.</p>
+			</div>
+
+			<div class="col-md-4 col-sm-6 ">
+				<h4>Helpful Links</h4>
+				<ul class="footer-links">
+					<li><a href="#">Login</a></li>
+					<li><a href="#">Sign Up</a></li>
+					<li><a href="#">My Account</a></li>
+					<li><a href="#">Add Property</a></li>
+					<li><a href="#">Pricing</a></li>
+					<li><a href="#">Privacy Policy</a></li>
+				</ul>
+
+				<ul class="footer-links">
+					<li><a href="#">FAQ</a></li>
+					<li><a href="#">Blog</a></li>
+					<li><a href="#">Our Agents</a></li>
+					<li><a href="#">How It Works</a></li>
+					<li><a href="#">Contact</a></li>
+				</ul>
+				<div class="clearfix"></div>
+			</div>
+
+			<div class="col-md-3  col-sm-12">
+				<h4>Contact Us</h4>
+				<div class="text-widget">
+					<span>12345 Little Lonsdale St, Melbourne</span> <br>
+					Phone: <span>(123) 123-456 </span><br>
+					E-Mail:<span> <a href="#"><span class="__cf_email__" data-cfemail="cda2ababa4aea88da8b5aca0bda1a8e3aea2a0">[email&#160;protected]</span></a> </span><br>
+				</div>
+
+				<ul class="social-icons margin-top-20">
+					<li><a class="facebook" href="#"><i class="icon-facebook"></i></a></li>
+					<li><a class="twitter" href="#"><i class="icon-twitter"></i></a></li>
+					<li><a class="gplus" href="#"><i class="icon-gplus"></i></a></li>
+					<li><a class="vimeo" href="#"><i class="icon-vimeo"></i></a></li>
+				</ul>
+
+			</div>
+
+		</div>
+
+		<!-- Copyright -->
+		<div class="row">
+			<div class="col-md-12">
+				<div class="copyrights">© 2016 Findeo. All Rights Reserved.</div>
+			</div>
+		</div>
+
+	</div>
+
+</div>
+<!-- Footer / End -->
+
+
+<!-- Back To Top Button -->
+<div id="backtotop"><a href="#"></a></div>
+
+
+<!-- Scripts
+================================================== -->
+<script data-cfasync="false" src="../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script type="text/javascript" src="scripts/jquery-2.2.0.min.js"></script>
+<script type="text/javascript" src="scripts/chosen.min.js"></script>
+<script type="text/javascript" src="scripts/magnific-popup.min.js"></script>
+<script type="text/javascript" src="scripts/owl.carousel.min.js"></script>
+<script type="text/javascript" src="scripts/rangeSlider.js"></script>
+<script type="text/javascript" src="scripts/sticky-kit.min.js"></script>
+<script type="text/javascript" src="scripts/slick.min.js"></script>
+<script type="text/javascript" src="scripts/mmenu.min.js"></script>
+<script type="text/javascript" src="scripts/tooltips.min.js"></script>
+<script type="text/javascript" src="scripts/masonry.min.js"></script>
+<script type="text/javascript" src="scripts/custom.js"></script>
+
+
+
+
+
+</div>
+
+</body>
+
+
+</html>
---
+++ property.php
@@ -0,0 +1,220 @@
+<?php include('inc/header.php'); ?>
+
+
+
+<!-- Titlebar
+================================================== -->
+<div id="titlebar" class="property-titlebar margin-bottom-0">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				
+				<a href="listings-list-with-sidebar.html" class="back-to-listings"></a>
+				<div class="property-title">
+					<h2>Selway Apartments <span class="property-badge">For Sale</span></h2>
+					<span>
+						<a href="#location" class="listing-address">
+							<i class="fa fa-map-marker"></i>
+							7843 Durham Avenue, MD
+						</a>
+					</span>
+				</div>
+
+				<div class="property-pricing">
+					<div class="property-price">$420,000</div>
+					<div class="sub-price">$770 / sq ft</div>
+				</div>
+
+
+			</div>
+		</div>
+	</div>
+</div>
+
+
+<!-- Content
+================================================== -->
+<div class="container">
+	<div class="row margin-bottom-50">
+		<div class="col-md-12">
+		
+			<!-- Slider -->
+			<div class="property-slider default">
+				<a href="images/single-property-01.jpg" data-background-image="images/single-property-01.jpg" class="item mfp-gallery"></a>
+				<a href="images/single-property-02.jpg" data-background-image="images/single-property-02.jpg" class="item mfp-gallery"></a>
+				<a href="images/single-property-03.jpg" data-background-image="images/single-property-03.jpg" class="item mfp-gallery"></a>
+				<a href="images/single-property-04.jpg" data-background-image="images/single-property-04.jpg" class="item mfp-gallery"></a>
+				<a href="images/single-property-05.jpg" data-background-image="images/single-property-05.jpg" class="item mfp-gallery"></a>
+				<a href="images/single-property-06.jpg" data-background-image="images/single-property-06.jpg" class="item mfp-gallery"></a>
+			</div>
+
+			<!-- Slider Thumbs -->
+			<div class="property-slider-nav">
+				<div class="item"><img src="images/single-property-01.jpg" alt=""></div>
+				<div class="item"><img src="images/single-property-02.jpg" alt=""></div>
+				<div class="item"><img src="images/single-property-03.jpg" alt=""></div>
+				<div class="item"><img src="images/single-property-04.jpg" alt=""></div>
+				<div class="item"><img src="images/single-property-05.jpg" alt=""></div>
+				<div class="item"><img src="images/single-property-06.jpg" alt=""></div>
+			</div>
+
+		</div>
+	</div>
+</div>
+
+
+<div class="container">
+	<div class="row">
+
+		<!-- Property Description -->
+		<div class="col-lg-8 col-md-7">
+			<div class="property-description">
+
+				<!-- Main Features -->
+				<ul class="property-main-features">
+					<li>Area <span>1450 sq ft</span></li>
+					<li>Rooms <span>4</span></li>
+					<li>Bedrooms <span>2</span></li>
+					<li>Bathrooms <span>1</span></li>
+				</ul>
+
+
+				<!-- Description -->
+				<h3 class="desc-headline">Description</h3>
+				<div class="show-more">
+					<p>
+						Ut euismod ultricies sollicitudin. Curabitur sed dapibus nulla. Nulla eget iaculis lectus. Mauris ac maximus neque. Nam in mauris quis libero sodales eleifend. Morbi varius, nulla sit amet rutrum elementum, est elit finibus tellus, ut tristique elit risus at metus. Sed fermentum, lorem vitae efficitur imperdiet, neque velit tristique turpis, et iaculis mi tortor finibus turpis. 
+					</p>
+
+					<p>
+						 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas in pulvinar neque. Nulla finibus lobortis pulvinar. Donec a consectetur nulla. Nulla posuere sapien vitae lectus suscipit, et pulvinar nisi tincidunt. Aliquam erat volutpat. Curabitur convallis fringilla diam sed aliquam. Sed tempor iaculis massa faucibus feugiat. In fermentum facilisis massa, a consequat purus viverra a. Aliquam pellentesque nibh et nibh feugiat gravida. Maecenas ultricies, diam vitae semper placerat, velit risus accumsan nisl, eget tempor lacus est vel nunc. Proin accumsan elit sed neque euismod fringilla. Curabitur lobortis nunc velit, et fermentum urna dapibus non. Vivamus magna lorem, elementum id gravida ac, laoreet tristique augue. Maecenas dictum lacus eu nunc porttitor, ut hendrerit arcu efficitur.
+					</p>
+
+					<p>
+						Nam mattis lobortis felis eu blandit. Morbi tellus ligula, interdum sit amet ipsum et, viverra hendrerit lectus. Nunc efficitur sem vel est laoreet, sed bibendum eros viverra. Vestibulum finibus, ligula sed euismod tincidunt, lacus libero lobortis ligula, sit amet molestie ipsum purus ut tortor. Nunc varius, dui et sollicitudin facilisis, erat felis imperdiet felis, et iaculis dui magna vitae diam. Donec mattis diam nisl, quis ullamcorper enim malesuada non. Curabitur lobortis eu mauris nec vestibulum. Nam efficitur, ex ac semper malesuada, nisi odio consequat dui, hendrerit vulputate odio dui vitae massa. Aliquam tortor urna, tincidunt ut euismod quis, semper vel ipsum. Ut non vestibulum mauris. Morbi euismod, felis non hendrerit viverra, nunc sapien bibendum ligula, eget vehicula nunc dolor eu ex. Quisque in semper odio. Donec auctor blandit ligula. Integer id lectus non nibh vulputate efficitur quis at arcu.
+					</p>
+
+					<a href="#" class="show-more-button">Show More <i class="fa fa-angle-down"></i></a>
+				</div>
+
+				<!-- Details -->
+				
+
+
+				<!-- Features -->
+				
+
+
+				<!-- Floorplans -->
+				
+
+				<!-- Video -->
+				
+
+				
+				<!-- Location -->
+				<h3 class="desc-headline no-border" id="location">Location</h3>
+				<div id="propertyMap-container">
+					<div id="propertyMap" data-latitude="40.7427837" data-longitude="-73.11445617675781"></div>
+					<a href="#" id="streetView">Street View</a>
+				</div>
+
+
+				<!-- Similar Listings Container -->
+				
+
+			</div>
+		</div>
+		<!-- Property Description / End -->
+
+
+		<!-- Sidebar -->
+		<div class="col-lg-4 col-md-5">
+			<div class="sidebar sticky right">
+
+				<!-- Widget -->
+				<div class="widget margin-bottom-30">
+					<button class="widget-button with-tip" data-tip-content="Print"><i class="sl sl-icon-printer"></i></button>
+					<button class="widget-button with-tip" data-tip-content="Add to Bookmarks"><i class="fa fa-star-o"></i></button>
+					<button class="widget-button with-tip compare-widget-button" data-tip-content="Add to Compare"><i class="icon-compare"></i></button>
+					<div class="clearfix"></div>
+				</div>
+				<!-- Widget / End -->
+
+
+				<!-- Widget -->
+				<div class="widget">
+
+					<!-- Agent Widget -->
+					<div class="agent-widget">
+						<div class="agent-title">
+							<div class="agent-photo"><img src="images/agent-avatar.jpg" alt="" /></div>
+							<div class="agent-details">
+								<h4><a href="#">Jennie Wilson</a></h4>
+								<span><i class="sl sl-icon-call-in"></i>(123) 123-456</span>
+							</div>
+							<div class="clearfix"></div>
+						</div>
+
+						<input type="text" placeholder="Your Email" pattern="^[A-Za-z0-9](([_\.\-]?[a-zA-Z0-9]+)*)@([A-Za-z0-9]+)(([\.\-]?[a-zA-Z0-9]+)*)\.([A-Za-z]{2,})$">
+						<input type="text" placeholder="Your Phone">
+						<textarea>I'm interested in this property [ID 123456] and I'd like to know more details.</textarea>
+						<button class="button fullwidth margin-top-5">Send Message</button>
+					</div>
+					<!-- Agent Widget / End -->
+
+				</div>
+				<!-- Widget / End -->
+
+
+				
+
+
+				<!-- Widget -->
+				<div class="widget">
+					<h3 class="margin-bottom-35">Featured Properties</h3>
+
+					<div class="listing-carousel outer">
+						<!-- Item -->
+						<div class="item">
+							<div class="listing-item compact">
+
+								<a href="#" class="listing-img-container">
+
+									<div class="listing-badges">
+										<span class="featured">Featured</span>
+										<span>For Sale</span>
+									</div>
+
+									<div class="listing-img-content">
+										<span class="listing-compact-title">Eagle Apartments <i>$275,000</i></span>
+
+										<ul class="listing-hidden-content">
+											<li>Area <span>530 sq ft</span></li>
+											<li>Rooms <span>3</span></li>
+											<li>Beds <span>1</span></li>
+											<li>Baths <span>1</span></li>
+										</ul>
+									</div>
+
+									<img src="images/listing-01.jpg" alt="">
+								</a>
+
+							</div>
+						</div>
+						<!-- Item / End -->
+
+						
+					</div>
+
+				</div>
+				<!-- Widget / End -->
+
+			</div>
+		</div>
+		<!-- Sidebar / End -->
+
+	</div>
+</div>
+
+<?php include('inc/footer.php'); ?>
